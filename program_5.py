4351f650e1012a06b0d372bc64c91ac72659f8c9
TheNiravPrajapati <niravprajapati2874@gmail.com>
2025-10-24 23:56:23 +0530

    Python Practice

--- program_5.py
+++ program_5.py
@@ -4,4 +4,16 @@ name2 = "Nirav"
 name3 = '''Nirav'''
 nameShort = name1[1:4:1]
 
-print(nameShort)
+# print(nameShort)
+
+name = input("Enter Your Name: \n")
+
+print(f"Good Evening: {name}")
+
+letter = '''
+    Dear <name>
+    You are the Awesome Developer and selected for google 
+    and Joining on <date>
+'''
+
+print(letter.replace("<name>", name).replace("<date>", '24-10-2025'))
---
+++ program_6.py
@@ -0,0 +1,3 @@
+frinds = ['Nirav', '12', 'python', 'larave']
+
+print(frinds[2])
