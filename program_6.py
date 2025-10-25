9cee0c5c97c971220cba0c607f09822878d7fda8
TheNiravPrajapati <niravprajapati2874@gmail.com>
2025-10-25 16:59:11 +0530

    Python Practice

--- program_6.py
+++ program_6.py
@@ -1,3 +1,13 @@
+# List: It is mutable, can be changed
 frinds = ['Nirav', '12', 'python', 'larave']
 
-print(frinds[2])
+print("list: " ,frinds[2])
+
+# Tuple : It is Imutable, Cant Be changed
+a = (1,2,4,4,5)
+
+print("Tuple: ", a)
+
+nos = a.count(4)
+
+print(nos)
