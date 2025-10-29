37bebd05a5fb72407d3db9869c765e86dd910eb8
TheNiravPrajapati <niravprajapati2874@gmail.com>
2025-10-29 12:48:17 +0530

    Python Practice

---
+++ program_10.py
@@ -0,0 +1,8 @@
+a = int(input("Enter Your Age: "))
+# if elif else Ladder
+if(a >= 18):
+    print("You Can Drive Your Vehicles")
+elif(a < 0):
+    print("You can not Enter the Invalid Age")
+else:
+    print("You Can not Drive")
--- program_9.py
+++ program_9.py
@@ -3,3 +3,18 @@ d = {}
 s = {1, 2, 3, 4, 5}
 
 e = set() # Empty Set
+
+print(s, type(s))
+
+# Set Methods
+s.add(6676)
+print(s, type(s))
+
+s.clear()
+print(s, type(s))
+
+s1 = {1,2,3,5,67,8,99,7,6,5,4}
+s2 = {6,5,23,53,532,5,3,53,5,6,6,6,8,9,0,}
+
+print(s1.union(s2))
+print(s1.intersection(s2))
