d3ee1454ce9b1758b48f23bbe2c6215205c49d5f
TheNiravPrajapati <niravprajapati2874@gmail.com>
2025-11-01 23:51:12 +0530

    Python Practice

---
+++ program_17.py
@@ -0,0 +1,17 @@
+# Function
+def avg():
+    a = int(input("Enter Number 1: "))
+    b = int(input("Enter Number 2: "))
+    c = int(input("Enter Number 3: "))
+
+    avg = (a+b+c)/3
+    print(avg)
+
+avg()
+
+def greetPerson(name1):
+    print(f"Good Day {name1}")
+
+greetPerson("Nirav")
+greetPerson("Raj")
+greetPerson("Mohan")
